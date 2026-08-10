358c352271d5f2fc3727e6c9634583b9d1a5474e
Mahbub <mrahman221441@gmail.com>
2026-08-10 14:20:19 +0600

    feat(url): store the limit state in redis

--- src/middlewares/url.middleware.js
+++ src/middlewares/url.middleware.js
@@ -1,12 +1,17 @@
 import { rateLimit } from 'express-rate-limit'
+import { RedisStore } from 'rate-limit-redis';
+
+import redis from "../lib/redisClient.js"
 
 const shortenLimit = rateLimit({
+    store: new RedisStore({
+        sendCommand: (...args) => redis.call(...args),
+    }),
     windowMs: 15 * 60 * 1000,
     max: 20,
     message: 'Too many links created from this IP, please try again later.',
     standardHeaders: true,
     legacyHeaders: false,
-    ipv6Subnet: 56,
 });
 
 const redirectLimit = rateLimit({
