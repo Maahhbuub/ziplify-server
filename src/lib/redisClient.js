11aa4e12bc13038d4c6f67c8e5bb65b3b52709e2
Mahbub <mrahman221441@gmail.com>
2026-09-01 16:36:31 +0600

    feat(add): connect redis to the cloude

--- src/lib/redisClient.js
+++ src/lib/redisClient.js
@@ -1,4 +1,13 @@
+// import { Redis } from 'ioredis';
+// const redis = new Redis();
+
+// export default redis;
+
 import { Redis } from 'ioredis';
-const redis = new Redis();
+
+const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
+
+redis.on('connect', () => console.log('Redis connected'));
+redis.on('error', (err) => console.error('Redis Client Error', err));
 
 export default redis;
