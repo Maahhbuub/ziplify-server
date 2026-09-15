5f2deba629a0f673ece10ae98137c25fc9fa1ae6
Mahbub <mrahman221441@gmail.com>
2026-09-16 02:09:14 +0600

    refactor(email): add space after please

--- src/templates/verification-email.js
+++ src/templates/verification-email.js
@@ -1,6 +1,6 @@
 const getVerificationEmailHtml = (verifyUrl) => `
-    <h2>Verify your email</h2>
-    <p>Please<a href="${verifyUrl}">click here</a> to verify your email address.</p>
+    <h2>Verify your ziplify account</h2>
+    <p>Please <a href="${verifyUrl}">click here</a> to verify your email address.</p>
 `;
 
 export { getVerificationEmailHtml };
