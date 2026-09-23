903906e40bec9e4aa5820dc0defcee500cf08af2
Mahbub <mrahman221441@gmail.com>
2026-09-23 22:13:56 +0600

    feat(app): deploy on render

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,7 +1,7 @@
 import appError from '../utils/appError.js';
 
 const globalError = (err, req, res, next) => {
-    console.log(err); //todo: delete it in production
+    // console.log(err); //todo: delete it in production
     const status = err.statusCode || 500;
     const message = err.message || 'Internal server error';
 
